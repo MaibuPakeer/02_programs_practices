804f80811e6d580d83bfec682402cf66d469bde5
kavithaganjiaja <k.ganji@ajacs.in>
2026-03-26 15:13:49 +0530

    Update by kavitha
    add two lines

--- fundamentals/A.java
+++ fundamentals/A.java
@@ -3,6 +3,7 @@ package fundamentals;
 public class A {
   public static void main(String[] args){
     System.out.println("hello mahoob");//this line written by kavitha
+    System.out.println("hello mahoob");//this line written by kavitha
   }
 
 }
